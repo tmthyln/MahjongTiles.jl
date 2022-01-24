4aed17e7a931da1c532e5ba1dea5c63835f80b0c
Documenter.jl <documenter@juliadocs.github.io>
2022-01-24 03:31:34 +0000

    build based on 8e54920

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MahjongTiles.jl-Documentation","page":"Home","title":"MahjongTiles.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
